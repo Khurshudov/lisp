b5757d2843a6e5884a1453e7bc6000d57ea50c79
Khurshudov <ar.kh.work@gmail.com>
2019-03-14 11:54:41 +0300

    Update 03_16.lisp

--- basics/03_16.lisp
+++ basics/03_16.lisp
@@ -1,13 +1,16 @@
-;Определите функцию, добавляющую элементы одного списка во второй спи- сок, начиная с заданной позиции.
+;Определите функцию, добавляющую элементы одного списка во второй спи- сок, начиная с заданной позиции.
 
-( defun insert (arr1 arr2 idx i res)
+( defun insert (arr1 arr2 idx)
     (cond
-        ((null arr1) res)
-        ((null arr2) (cons (car arr1) (insert (cdr arr1) arr2 idx (+ 1 i) res) ))
-        ((>= i (- idx 1)) (cons (car arr2) (insert arr1 (cdr arr2) idx i res) ))
-        (t (cons (car arr1) (insert (cdr arr1) arr2 idx (+ 1 i) res) ))
+        ((and 
+              (null arr1) 
+              (null arr2)
+         ) NIL)
+        ((> idx 1) (cons (car arr1) (insert (cdr arr1) arr2 (1- idx))))
+        ((not (null arr2)) (cons (car arr2) (insert arr1 (cdr arr2) -1)))
+        (t (cons (car arr1) (insert (cdr arr1) arr2 -1)))
     )
 )
 
-(print (insert '(1 2 3 4 5) '(10 20 30 40 50) 2 0 NIL)) ;(1 10 20 30 40 50 2 3 4 5) 
-(print (insert '(1 2 3 4 5) NIL 2 0 NIL)) ;(1 2 3 4 5)
+(print (insert '(1 2 3 4 5) '(10 20 30 40 50) 2)) ;(1 10 20 30 40 50 2 3 4 5) 
+(print (insert '(1 2 3 4 5) NIL 2)) ;(1 2 3 4 5)
