14ef05ea80451bec94abe48655bbfa3579ca17f1
Khurshudov <ar.kh.work@gmail.com>
2019-03-14 14:15:55 +0300

    Update 02_11.lisp

--- basics/02_11.lisp
+++ basics/02_11.lisp
@@ -1,20 +1,16 @@
-;Определите функцию, осуществляющую разделение исходного списка на два подсписка. В первый из них должно попасть указанное количество элементов с начала списка, во второй — оставшиеся элементы.
+;Определите функцию, осуществляющую разделение исходного списка на два подсписка. 
+;В первый из них должно попасть указанное количество элементов с начала списка,
+;во второй — оставшиеся элементы.
 
-(defun appen (l1 l2)
+(defun split (arr i)
     (cond
-        ( (null l1) l2 )
-        ( t (cons (car l1) (appen (CDR L1) l2 ) ))
+        ( (null arr) (list NIL NIL))
+        ((> i 0)  ((lambda (x) (list (cons (car arr) (car x)) (cadr x)))   (split (cdr arr) (1- i)) ) )
+        (t  ((lambda (x) (list (car x) (cons (car arr) (cadr x))))   (split (cdr arr) (1- i)) ) )
     )
 )
 
-(defun split (arr first last i)
-    (cond
-        ( (null arr) (list first last))
-        ( (> i 0) (split (cdr arr) (appen first (list (car arr))) last (- i 1)) )
-        ( t (split (cdr arr) first (appen last (list (car arr))) (- i 1)))
-    )
-)
-
-(print (split '( -10 1 2 3 4 5 -2 -4) NIL NIL 3)) ; ((-10 1 2) (3 4 5 -2 -4)) 
-(print (split '( -10 1) NIL NIL 3)) ; ((-10 1) NIL) 
-(print (split '( -10 1 1 1) NIL NIL 3)) ; ((-10 1 1) (1))
+(print (split '( -10 1 2 3 4 5 -2 -4) 3)) ; ((-10 1 2) (3 4 5 -2 -4)) 
+(print (split '( -10 1) 3)) ; ((-10 1) NIL) 
+(print (split '( -10 1 1 1) 3)) ; ((-10 1 1) (1))
+(print (split '( -10 1 1 1) 0)) ; (NIL (-10 1 1 1)) 
