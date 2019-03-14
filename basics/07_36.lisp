a44ecb50e5bacbd8be6c582feb6bad03906ddfb7
Khurshudov <ar.kh.work@gmail.com>
2019-03-14 09:38:13 +0200

    Update 07_36.lisp

--- basics/07_36.lisp
+++ basics/07_36.lisp
@@ -1,33 +1,17 @@
 ;Определите предикат НЕПЕРЕСЕКАЮЩИЕСЯ, проверяющий, что два множества не пересекаются, т.е. у них нет общих элементов.
 
-(defun present (el arr)
+(defun ismember (el arr)
     (cond
         ((null arr) NIL)
         ((= (car arr) el) T)
-        (t (present el (cdr arr)))
+        (t (ismember el (cdr arr)))
     )
 )
-
-;(defun independent (arr1 arr2)
-;    (cond
-;        ((null arr1) t)
-;        ((null ((lambda (el arr) (cond
-;                                     ((null arr) NIL)
-;                                     ((= (car arr) el) T)
-;                                     (t (present el (cdr arr)))
-;                                 )
-;                ) (car arr1) arr2
-;               )
-;          ) (independent (cdr arr1) arr2)
-;         )
-;        (t NIL)
-;    )
-;)
 
 (defun independent (arr1 arr2)
     (cond
         ((null arr1) t)
-        ((null (present (car arr1) arr2)) (independent (cdr arr1) arr2))
+        ((null (ismember (car arr1) arr2)) (independent (cdr arr1) arr2))
         (t NIL)
     )
 )
