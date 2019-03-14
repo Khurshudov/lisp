5a8bd1718293a3c8862142b4d289bc322e0a83dd
Khurshudov <ar.kh.work@gmail.com>
2019-03-14 09:35:44 +0200

    Update 05_26.lisp

--- basics/05_26.lisp
+++ basics/05_26.lisp
@@ -1,10 +1,12 @@
 ;Определите функцию, разбивающую список (a b с d...) на пары ((а b) (с d)...).
-
-( defun split (arr res)
+( defun split (arr)
     (cond
-        ((null arr) res)
-        (t (cons ( cons (car arr) (list (cadr arr))) (split (cddr arr) res) ))
+        ((null arr) NIL)
+        (t (cons ( cons (car arr) (list (cadr arr))) (split (cddr arr)) ))
     )
 )
 
-(print (split '(1 2 3 4 5) NIL))
+(print (split '(1 2 3 4 5))) ; ((1 2) (3 4) (5 NIL))
+(print (split '(1 2 3 4 5 6))) ; ((1 2) (3 4) (5 6)) 
+(print (split NIL)) ; NIL
+(print (split '(1))) ; ((1 NIL)) 
