06769120ac2c842f362c152ce3ffbfa3de2c3cfc
Khurshudov <ar.kh.work@gmail.com>
2019-03-14 11:59:10 +0300

    Update 06_29.lisp

--- basics/06_29.lisp
+++ basics/06_29.lisp
@@ -1,22 +1,18 @@
 ;Определите функцию, вычисляющую глубину списка (самой глубокой ветви).
 
-(defun incr (a)
-    (+ a 1)
-)
-
-( defun depth (arr cur_d)
+( defun depth (arr)
     (cond
-        ((null arr) cur_d)
+        ((null arr) 0)
         (t ((lambda (a b) (cond 
                               ((> a b) a)
                               (t b)
                           )) 
-               (depth (cadr arr) (incr cur_d)) 
-               (depth (caddr arr) (incr cur_d)))
+               (1+ (depth (cadr arr)))
+               (1+ (depth (caddr arr))))
            )
     )
 )
 
-(print (depth '(A NIL (B (C NIL NIL) NIL)) 0)) ; 3
-(print (depth NIL 0)) ; 0
-(print (depth '(1 NIL NIL) 0)) ; 1
+(print (depth '(A NIL (B (C NIL NIL) NIL)))) ; 3
+(print (depth NIL)) ; 0
+(print (depth '(1 NIL NIL))) ; 1
