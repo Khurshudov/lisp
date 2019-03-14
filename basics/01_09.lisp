84107d169ee1f0e01b18aecf7e6c45c74faa3dcb
Khurshudov <ar.kh.work@gmail.com>
2019-03-14 10:06:28 +0200

    Update 01_09.lisp

--- basics/01_09.lisp
+++ basics/01_09.lisp
@@ -1,21 +1,18 @@
-;Определите функцию, разделяющую исходный список на два подсписка. В первый из них должны попасть элементы с нечетными номерами, во второй — элементы с четными номерами.
-
-
-(defun appen (l1 l2)
+;Определите функцию, разделяющую исходный список на два подсписка. В первый из них должны попасть элементы 
+;с нечетными номерами,во второй — элементы с четными номерами.
+(defun split (arr)
     (cond
-        ( (null l1) l2 )
-        ( t (cons (car l1) (appen (CDR L1) l2 ) ))
+        ( (null arr) (list NIL NIL))
+        ((null (cadr arr)) (list (list (car arr)) NIL))
+        (T (list 
+                (cons (car arr) (car (split (cddr arr)))) 
+                (cons (cadr arr) (cadr (split (cddr arr)))) 
+           )
+        )
+        
     )
 )
 
-(defun split (arr odd even i)
-    (cond
-        ( (null arr) (list odd even))
-        ( (= 1(mod i 2)) (split (cdr arr) (appen odd (list (car arr))) even (+ i 1)) )
-        ( t (split (cdr arr) odd (appen even (list (car arr))) (+ i 1)))
-    )
-)
-
-(print (split '( -10 1 2 3 4 5 -2 -4) NIL NIL 1)) ;((-10 2 4 -2) (1 3 5 -4)) 
-(print (split '( -10) NIL NIL 1)) ;((-10) NIL) 
-(print (split NIL NIL NIL 1)) ;(NIL NIL) 
+(print (split '( 1 2 3 4 5 6 7 8 9 ))) ;((-10 2 4 -2) (1 3 5 -4)) 
+(print (split '( -10))) ;((-10) NIL) 
+(print (split NIL)) ;(NIL NIL) 
