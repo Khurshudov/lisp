89438959bef54f767c642a8dee15f76362fb218f
Артем Хуршудов <khurshudov@MacBook-Pro-Artem.local>
2019-03-20 21:14:46 +0300

    refactoring

--- basics/03_16.lisp
+++ basics/03_16.lisp
@@ -1,16 +1,16 @@
 ;Определите функцию, добавляющую элементы одного списка во второй спи- сок, начиная с заданной позиции.
 
-( defun insert (arr1 arr2 idx)
+( defun insert (&key to from pos)
     (cond
         ((and 
-              (null arr1) 
-              (null arr2)
+              (null to) 
+              (null from)
          ) NIL)
-        ((> idx 1) (cons (car arr1) (insert (cdr arr1) arr2 (1- idx))))
-        ((not (null arr2)) (cons (car arr2) (insert arr1 (cdr arr2) -1)))
-        (t (cons (car arr1) (insert (cdr arr1) arr2 -1)))
+        ((> pos 1) (cons (car to) (insert :to (cdr to) :from from :pos (1- pos))))
+        ((not (null from)) (cons (car from) (insert :to to :from (cdr from) :pos -1)))
+        (t (cons (car to) (insert :to (cdr to) :from from :pos -1)))
     )
 )
 
-(print (insert '(1 2 3 4 5) '(10 20 30 40 50) 2)) ;(1 10 20 30 40 50 2 3 4 5) 
-(print (insert '(1 2 3 4 5) NIL 2)) ;(1 2 3 4 5)
+(print (insert :to '(1 2 3 4 5) :from '(10 20 30 40 50) :pos 2)) ;(1 10 20 30 40 50 2 3 4 5) 
+(print (insert :to '(1 2 3 4 5) :from NIL :pos 2)) ;(1 2 3 4 5)
