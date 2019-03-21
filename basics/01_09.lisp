ff1ef08ebe5e5c9dfef51595aee0d20a2231202f
Khurshudov <ar.kh.work@gmail.com>
2019-03-21 09:31:37 +0200

    Update 01_09.lisp

--- basics/01_09.lisp
+++ basics/01_09.lisp
@@ -17,6 +17,6 @@
     )
 )
 
-(print (split '( 1 2 3 4 5 6 7 8 9 ))) ;((-10 2 4 -2) (1 3 5 -4)) 
+(print (split '( 1 2 3 4 5 6 7 8 9 ))) ;((1 3 5 7 9) (2 4 6 8)) 
 (print (split '( -10))) ;((-10) NIL) 
 (print (split NIL)) ;(NIL NIL) 
