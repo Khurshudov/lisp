6919ca4039e8d2517ceb21326664ce12f50250d1
Артем Хуршудов <khurshudov@MacBook-Pro-Artem.local>
2019-04-17 16:50:04 +0300

    added 10

---
+++ 2/10.lisp
@@ -0,0 +1,25 @@
+(defun gen ()
+    (let ((arr NIL) )
+        (lambda ()
+            (
+                (lambda (el)
+                    (setq arr (cons el arr))
+                ) (cond
+                    ((eq (car arr) 'A) 'B)
+                    (T 'A)
+                  ) 
+            )
+
+        )
+    )
+)
+
+(setq g1 (gen))
+(setq g2 (gen))
+
+(print (funcall g1)) ; (A) 
+(print (funcall g1)) ; (B A) 
+(print (funcall g1)) ; (A B A) 
+(print (funcall g1)) ; (B A B A) 
+(print (funcall g2)) ; (A) 
+(print (funcall g2)) ; (B A) 
